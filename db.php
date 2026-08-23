1107183813135baceaea6829480204db547b314e
osakioyadavidosaze <osakioyadavid571@gmail.com>
2026-08-23 22:30:02 +0100

    Update db.php

--- db.php
+++ db.php
@@ -89,15 +89,13 @@ function db(): PDO
         '🥗',
         'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&auto=format&fit=crop&w=1170&q=80'
     ]
-       
-    [
-        'Smoky Garden Burger',
-        'Charred plant-based patty, tomato jam, crisp lettuce and herb aioli.',
-        'Mains',
-        14.00,
-        '🍔',
-        'https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&auto=format&fit=crop&w=1170&q=80'
-    ],
+      [
+    'Smoky Garden Burger',
+    'Charred plant-based patty, tomato jam, crisp lettuce and herb aioli.',
+    'Mains',
+    14.00,
+    '🍔'
+],
     [
         'Citrus Spark',
         'Fresh orange, lime, ginger and a bright splash of sparkling water.',
